4eb64c47ce326f74f0e8bf642f5ae91eee6b0342
Kacper Slodzinski <ksodziski@gmail.com>
2018-07-24 10:50:34 +0200

    final commit

--- src/main/java/pl/akademiakodu/chuckNorrisJokes/HomeController.java
+++ src/main/java/pl/akademiakodu/chuckNorrisJokes/HomeController.java
@@ -1,4 +1,17 @@
 package pl.akademiakodu.chuckNorrisJokes;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(ModelMap modelMap) {
+        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+        modelMap.put("quote", chuckNorrisQuotes.getRandomQuote());
+        return "home";
+    }
 }
